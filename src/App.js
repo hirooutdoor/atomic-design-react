39d5eb7de7b440a0e683ecb0a13904313f50afc9
hiro <hmaeyama0510@gmail.com>
2021-05-28 09:53:20 +0900

    Create Molecule

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { PrimaryButton } from "./compoenents/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./compoenents/atoms/button/SecondaryButton";
+import { SearchInput } from "./compoenents/molecules/SearchInput";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <PrimaryButton>Test</PrimaryButton>
       <SecondaryButton>Search</SecondaryButton>
+      <SearchInput />
     </div>
   );
 }
