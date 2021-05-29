56c4684895411a545ea9b6c712a7c531faf53619
hiro <hmaeyama0510@gmail.com>
2021-05-29 13:21:52 +0900

    modify users page

--- src/compoenents/pages/Users.jsx
+++ src/compoenents/pages/Users.jsx
@@ -1,13 +1,18 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
 
 export const Users = () => {
   return (
     <SContainer>
-      <h2>Users Index page</h2>
+      <h2>All Users</h2>
+      <SearchInput />
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `;
