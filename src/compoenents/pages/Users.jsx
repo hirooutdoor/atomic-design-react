e77fa2f14e716f41f3dc96048d162cf415cea266
hiro <hmaeyama0510@gmail.com>
2021-05-29 19:27:33 +0900

    add global context and change button

--- src/compoenents/pages/Users.jsx
+++ src/compoenents/pages/Users.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
+import { UserContext } from "../../providers/UserProvider";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -19,16 +20,19 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const onClickSwich = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
 
   return (
     <SContainer>
       <h2>All Users</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwich}>Change</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+          <UserCard key={user.id} user={user} />
         ))}
       </SUserArea>
     </SContainer>
