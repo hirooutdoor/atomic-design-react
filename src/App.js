5baa0d5765fbc51a3f9fe5349c31ed07dbaf51d4
hiro <hmaeyama0510@gmail.com>
2021-05-29 13:32:35 +0900

    duplicated user card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "Armin",
-  image: "https://source.unsplash.com/ux2LvytLQ-A",
-  email: "12345@example.com",
-  phone: "000-000-000",
-  company: {
-    name: "Alphabet.LTD"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/compoenents/pages/Users.jsx
+++ src/compoenents/pages/Users.jsx
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `Armin-${val}`,
+    image: "https://source.unsplash.com/ux2LvytLQ-A",
+    email: "12345@example.com",
+    phone: "000-000-000",
+    company: {
+      name: "Alphabet.LTD"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>All Users</h2>
       <SearchInput />
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </SContainer>
   );
 };
